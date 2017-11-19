16794eba8a8a5793d66b87f5beb32ab616d72577
Verta-master <vertyagina@gmail.com>
2017-11-19 21:05:43 +0600

    Functions

--- js/stat.js
+++ js/stat.js
@@ -22,77 +22,49 @@ window.renderStatistics = function (ctx, names, times) {
   var halfX = Math.round(cloudData.startX + cloudData.width / 2);
   var halfY = Math.round(cloudData.startY + cloudData.height / 2);
 
+  function drawCloud(shift) {
+    ctx.beginPath();
+    ctx.moveTo(cloudData.startX + shift, cloudData.startY + shift);
+    ctx.lineTo(thirdX + shift, cloudData.startY + shift);
+    ctx.bezierCurveTo(
+        halfX + shift, cloudData.startY - cloudData.arcSize + shift,
+        halfX + shift, cloudData.startY - cloudData.arcSize + shift,
+        thirdX * 2 - cloudData.startX + shift, cloudData.startY + shift
+    );
+    ctx.lineTo(cloudData.startX + cloudData.width + shift, cloudData.startY + shift);
+    ctx.lineTo(cloudData.startX + cloudData.width + shift, thirdY + shift);
+    ctx.bezierCurveTo(
+        cloudData.startX + cloudData.width + cloudData.arcSize + shift, halfY + shift,
+        cloudData.startX + cloudData.width + cloudData.arcSize + shift, halfY + shift,
+        cloudData.startX + cloudData.width + shift, thirdY * 2 - cloudData.startY + shift
+    );
+    ctx.lineTo(cloudData.startX + cloudData.width + shift, cloudData.startY + cloudData.height + shift);
+    ctx.lineTo(thirdX * 2 - cloudData.startX + shift, cloudData.startY + cloudData.height + shift);
+    ctx.bezierCurveTo(
+        halfX + shift, cloudData.startY + cloudData.height + cloudData.arcSize + shift,
+        halfX + shift, cloudData.startY + cloudData.height + cloudData.arcSize + shift,
+        thirdX + shift, cloudData.startY + cloudData.height + shift
+    );
+    ctx.lineTo(cloudData.startX + shift, cloudData.startY + cloudData.height + shift);
+    ctx.lineTo(cloudData.startX + shift, thirdY * 2 - cloudData.startY + shift);
+    ctx.bezierCurveTo(
+        cloudData.startX - cloudData.arcSize + shift, halfY + shift,
+        cloudData.startX - cloudData.arcSize + shift, halfY + shift,
+        cloudData.startX + shift, thirdY + shift
+    );
+    ctx.lineTo(cloudData.startX + shift, cloudData.startY + shift);
+    ctx.closePath();
+    ctx.stroke();
+    ctx.fill();
+  }
+
   // cloud's shadow
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.beginPath();
-  ctx.moveTo(cloudData.startX + cloudData.shadowShift, cloudData.startY + cloudData.shadowShift);
-  ctx.lineTo(thirdX + cloudData.shadowShift, cloudData.startY + cloudData.shadowShift);
-  ctx.bezierCurveTo(
-      halfX + cloudData.shadowShift, cloudData.startY - cloudData.arcSize + cloudData.shadowShift,
-      halfX + cloudData.shadowShift, cloudData.startY - cloudData.arcSize + cloudData.shadowShift,
-      thirdX * 2 - cloudData.startX + cloudData.shadowShift, cloudData.startY + cloudData.shadowShift
-  );
-  ctx.lineTo(cloudData.startX + cloudData.width + cloudData.shadowShift, cloudData.startY + cloudData.shadowShift);
-  ctx.lineTo(cloudData.startX + cloudData.width + cloudData.shadowShift, thirdY + cloudData.shadowShift);
-  ctx.bezierCurveTo(
-      cloudData.startX + cloudData.width + cloudData.arcSize + cloudData.shadowShift, halfY + cloudData.shadowShift,
-      cloudData.startX + cloudData.width + cloudData.arcSize + cloudData.shadowShift, halfY + cloudData.shadowShift,
-      cloudData.startX + cloudData.width + cloudData.shadowShift, thirdY * 2 - cloudData.startY + cloudData.shadowShift
-  );
-  ctx.lineTo(cloudData.startX + cloudData.width + cloudData.shadowShift, cloudData.startY + cloudData.height + cloudData.shadowShift);
-  ctx.lineTo(thirdX * 2 - cloudData.startX + cloudData.shadowShift, cloudData.startY + cloudData.height + cloudData.shadowShift);
-  ctx.bezierCurveTo(
-      halfX + cloudData.shadowShift, cloudData.startY + cloudData.height + cloudData.arcSize + cloudData.shadowShift,
-      halfX + cloudData.shadowShift, cloudData.startY + cloudData.height + cloudData.arcSize + cloudData.shadowShift,
-      thirdX + cloudData.shadowShift, cloudData.startY + cloudData.height + cloudData.shadowShift
-  );
-  ctx.lineTo(cloudData.startX + cloudData.shadowShift, cloudData.startY + cloudData.height + cloudData.shadowShift);
-  ctx.lineTo(cloudData.startX + cloudData.shadowShift, thirdY * 2 - cloudData.startY + cloudData.shadowShift);
-  ctx.bezierCurveTo(
-      cloudData.startX - cloudData.arcSize + cloudData.shadowShift, halfY + cloudData.shadowShift,
-      cloudData.startX - cloudData.arcSize + cloudData.shadowShift, halfY + cloudData.shadowShift,
-      cloudData.startX + cloudData.shadowShift, thirdY + cloudData.shadowShift
-  );
-  ctx.lineTo(cloudData.startX + cloudData.shadowShift, cloudData.startY + cloudData.shadowShift);
-  ctx.closePath();
-  ctx.stroke();
-  ctx.fill();
+  drawCloud(cloudData.shadowShift);
 
   // white cloud
   ctx.fillStyle = 'rgba(256, 256, 256, 1.0)';
-  ctx.beginPath();
-  ctx.moveTo(cloudData.startX, cloudData.startY);
-  ctx.lineTo(thirdX, cloudData.startY);
-  ctx.bezierCurveTo(
-      halfX, cloudData.startY - cloudData.arcSize,
-      halfX, cloudData.startY - cloudData.arcSize,
-      thirdX * 2 - cloudData.startX, cloudData.startY
-  );
-  ctx.lineTo(cloudData.startX + cloudData.width, cloudData.startY);
-  ctx.lineTo(cloudData.startX + cloudData.width, thirdY);
-  ctx.bezierCurveTo(
-      cloudData.startX + cloudData.width + cloudData.arcSize, halfY,
-      cloudData.startX + cloudData.width + cloudData.arcSize, halfY,
-      cloudData.startX + cloudData.width, thirdY * 2 - cloudData.startY
-  );
-  ctx.lineTo(cloudData.startX + cloudData.width, cloudData.startY + cloudData.height);
-  ctx.lineTo(thirdX * 2 - cloudData.startX, cloudData.startY + cloudData.height);
-  ctx.bezierCurveTo(
-      halfX, cloudData.startY + cloudData.height + cloudData.arcSize,
-      halfX, cloudData.startY + cloudData.height + cloudData.arcSize,
-      thirdX, cloudData.startY + cloudData.height
-  );
-  ctx.lineTo(cloudData.startX, cloudData.startY + cloudData.height);
-  ctx.lineTo(cloudData.startX, thirdY * 2 - cloudData.startY);
-  ctx.bezierCurveTo(
-      cloudData.startX - cloudData.arcSize, halfY,
-      cloudData.startX - cloudData.arcSize, halfY,
-      cloudData.startX, thirdY
-  );
-  ctx.lineTo(cloudData.startX, cloudData.startY);
-  ctx.closePath();
-  ctx.stroke();
-  ctx.fill();
+  drawCloud(0);
 
   // Message on the cloud
   ctx.fillStyle = '#000000';
@@ -119,24 +91,35 @@ window.renderStatistics = function (ctx, names, times) {
 
   // Histogram
   var step = histogramData.histogramHeight / getMaxFromArray(times);
-  for (var i = 0; i < times.length; i++) {
+
+  function drawBar(item, i) {
     ctx.fillStyle = getColorPlayer(names[i]);
     ctx.fillRect(
         histogramData.initialX + (histogramData.barWidth + histogramData.indent) * i,
-        histogramData.histogramHeight - times[i] * step + histogramData.initialY,
+        histogramData.histogramHeight - item * step + histogramData.initialY,
         histogramData.barWidth,
-        times[i] * step
+        item * step
     );
+  }
+
+  function typeNames(item, i) {
     ctx.fillStyle = 'rgba(0, 0, 0, 1.0)';
     ctx.fillText(
-        names[i],
+        item,
         histogramData.initialX + (histogramData.barWidth + histogramData.indent) * i,
         histogramData.histogramHeight + histogramData.lineHeight + histogramData.initialY
     );
+  }
+
+  function typeTime(item, i) {
     ctx.fillText(
-        Math.round(times[i]),
+        Math.round(item),
         histogramData.initialX + (histogramData.barWidth + histogramData.indent) * i,
-        histogramData.histogramHeight - times[i] * step + histogramData.initialY - histogramData.lineHeight
+        histogramData.histogramHeight - item * step + histogramData.initialY - histogramData.lineHeight
     );
   }
+
+  times.forEach(drawBar);
+  names.forEach(typeNames);
+  times.forEach(typeTime);
 };
